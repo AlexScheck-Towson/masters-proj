b7ae5f33f505bbacdc4bba130630acaba3378dd5
Alex Scheck <aschec2@students.towson.edu>
2020-07-26 14:57:07 -0400

    added actual default stop words

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -11,50 +11,55 @@ export class MainService {
 
   constructor(private http: HttpClient) { }
 
-  private flashURL:string = 'http://127.0.0.1:5002/';
-  public passes:number[] = []
+  private flashURL: string = 'http://127.0.0.1:5002/';
+  public passes: number[] = []
 
   private defaultNumTopics = 13;
   private defaultAlpha = 30;
-  private defaultStopWords = "module, student, james";
+  private defaultStopWords = 'introduce, module, introduction, issue, cover, overview, topic, concept, '
+    + 'material, focus, student, process, give, basic, information, include, computer, operation, lesson, '
+    + 'state, hour, base, problem, build, identify, presentation, common, present, micromodule, discuss, teach, '
+    + 'class, learn, exercise, lecture, activity, method, important, apply, step, create, explain, environment, '
+    + 'complete, hand, review, nanomodule, unit, also, technique, tool, perform, principle, understand, '
+    + 'challenge, understanding, intend, topic, implement, provide, application, layer, cybersecurity';
   private lastRunNumTopics = this.defaultNumTopics;
   private lastRunAlpha = this.defaultAlpha;
   private lastRunStopWords = this.defaultStopWords;
 
-  private getUrl(path:string) {
+  private getUrl(path: string) {
     return 'http://127.0.0.1:5002/' + path;
   }
 
   loadPasses() {
-    this.http.get<number[]>(this.getUrl('passNums')).subscribe(data =>{
+    this.http.get<number[]>(this.getUrl('passNums')).subscribe(data => {
       this.passes = data;
     });
   }
 
-  getTopicsForPass(passNum:number):Observable<Topic[]> {
-    return this.http.post<Topic[]>(this.getUrl('topics'), {passNum: passNum});
+  getTopicsForPass(passNum: number): Observable<Topic[]> {
+    return this.http.post<Topic[]>(this.getUrl('topics'), { passNum: passNum });
   }
 
-  getLearningObjectsForPassAndTopic(passNum: number, topicNum:number):Observable<LearningObject[]> {
-    return this.http.post<LearningObject[]>(this.getUrl('learning-objects'), {passNum: passNum, topicNum: topicNum});
+  getLearningObjectsForPassAndTopic(passNum: number, topicNum: number): Observable<LearningObject[]> {
+    return this.http.post<LearningObject[]>(this.getUrl('learning-objects'), { passNum: passNum, topicNum: topicNum });
   }
 
-  clearAllPasses():Observable<boolean> {
+  clearAllPasses(): Observable<boolean> {
     return this.http.get<boolean>(this.getUrl('clearPasses'));
   }
 
-  executeTopicModeling(numTopics:number, alpha:number, stopWords:string[], stopWordsString:string):Observable<number> {
+  executeTopicModeling(numTopics: number, alpha: number, stopWords: string[], stopWordsString: string): Observable<number> {
     this.lastRunNumTopics = numTopics;
     this.lastRunAlpha = alpha;
     this.lastRunStopWords = stopWordsString;
-    return this.http.post<number>(this.getUrl('execute'), {numTopics: numTopics, alpha: alpha, stopWords: stopWords});
+    return this.http.post<number>(this.getUrl('execute'), { numTopics: numTopics, alpha: alpha, stopWords: stopWords });
   }
 
-  populatePasses(passes:number[]) {
+  populatePasses(passes: number[]) {
     this.passes = passes;
   }
 
-  addPass(pass:number) {
+  addPass(pass: number) {
     this.passes.push(pass);
   }
 
@@ -82,4 +87,3 @@ export class MainService {
     return this.lastRunStopWords;
   }
 }
- 
